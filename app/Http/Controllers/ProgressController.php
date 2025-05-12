6f8d5abba1c390e94b2f72f88f55dcbe9b8e5013
Henry Lee <henryleeworld@gmail.com>
2025-05-13 00:00:57 +0800

    feat:升級至 laravel 11

--- app/Http/Controllers/ProgressController.php
+++ app/Http/Controllers/ProgressController.php
@@ -14,25 +14,26 @@ class ProgressController extends Controller
      */
     public function step()
     {
-        $step1 = (new Step('https://github.com/henryleeworld', 'Github Site'))->url();
-        $step2 = (new Step('北極程式碼保險櫃貢獻者', 'Highlights'))->string();
+        $step1 = (new Step('https://github.com/henryleeworld', __('Github Site')))->url();
+        $step2 = (new Step(__('Arctic Code Vault Contributor'), __('Highlights')))->string();
         $progress = (new Progress($step1, $step2));
-        echo '總步驟數：' . $progress->get()['total_steps'] . PHP_EOL;
-		echo '完成百分比：' . $progress->get()['percentage_complete'] . PHP_EOL;
-		echo '未完成百分比：' . $progress->get()['percentage_incomplete'] . PHP_EOL;
-		echo '步驟完成數：' . $progress->get()['steps_complete'] . PHP_EOL;
-		echo '步驟未完成數：' . $progress->get()['steps_incomplete'] . PHP_EOL;
+        echo __('Total steps: ') . $progress->get()['total_steps'] . PHP_EOL . PHP_EOL;
+		echo __('Percentage complete: ') . $progress->get()['percentage_complete'] . PHP_EOL;
+		echo __('Percentage incomplete: ') . $progress->get()['percentage_incomplete'] . PHP_EOL . PHP_EOL;
+		echo __('Steps complete: ') . $progress->get()['steps_complete'] . PHP_EOL;
+		echo __('Steps incomplete: ') . $progress->get()['steps_incomplete'] . PHP_EOL . PHP_EOL;
         if (count($progress->get()['complete_step_names'])) {
-            echo '步驟完成內容：' . PHP_EOL;
+            echo __('Complete step contents: ') . PHP_EOL;
         }
         foreach ($progress->get()['complete_step_names'] as $key => $name) {
-		    echo '步驟完成名稱' . ($key + 1) . '：' . $name . PHP_EOL;
+		    echo __('Complete step names') . ' ' . ($key + 1) . __('： ') . $name . PHP_EOL;
         }
         if (count($progress->get()['incomplete_step_names'])) {
-            echo '步驟未完成內容：' . PHP_EOL;
+            echo PHP_EOL;
+            echo __('Incomplete step contents: ') . PHP_EOL;
         }
         foreach ($progress->get()['incomplete_step_names'] as $key => $name) {
-		    echo '步驟未完成名稱' . ($key + 1) . '：' . $name . PHP_EOL;
+		    echo __('Incomplete step names') . ' ' . ($key + 1) . '：' . $name . PHP_EOL;
         }
     }
 }
